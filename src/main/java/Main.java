106d2f161b2799b1e3ee5587e5949bf12c89e270
EvgeniyOvch <ovchinnicov@gmail.com>
2023-03-28 13:29:24 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(6,7));
+        System.out.println(calc.mult(15,5));
+        System.out.println(calc.pow(10,2));
+    }
+}
